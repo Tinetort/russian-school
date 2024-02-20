27b567a1a7641630bf178d198a6cbf527dc6b07e
Alexander Volkov <jkbitp@gmail.com>
2024-02-20 00:41:38 -0800

    metadataBase added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,13 @@ import './globals.css'
 import '../../bootstrap.min.css'
 import { Montserrat } from 'next/font/google'
 
-
 const inter = Montserrat({ subsets: ['latin'] })
 
-export const metadata  = {
+export const metadata = {
     title: 'San Diego Russian School',
     description:
         'Discover San Diego Russian School, where children explore the Russian language and culture in a vibrant educational environment. Join us for engaging language lessons, cultural activities, and fun learning experiences.',
+        metadataBase: new URL('https://russian-school.vercel.app'),
 }
 
 export default function RootLayout({ children }) {
